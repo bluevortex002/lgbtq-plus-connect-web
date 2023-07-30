973407e706b28de64f460f6a125c2b42ebc79e3e
Jin Hui Zhang <zjhken@outlook.com>
2023-07-30 00:28:57 +0000

    update svg

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,50 +1,10 @@
 import { For, type Component } from 'solid-js';
 import { conversationsSignal } from '../context';
+import forum from "../svg/forum.svg"
 
 const ChatPage: Component = () => {
 
 	const [conversations, setConversations] = conversationsSignal
-
-	const c = <For each={conversations()}>
-		{(conv) => <li>
-			<a
-				class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none">
-				<img class="object-cover w-10 h-10 rounded-full"
-					src={conv.user?.avatarUrl} alt="username" />
-				<div class="w-full pb-2">
-					<div class="flex justify-between">
-						<span class="block ml-2 font-semibold text-gray-600">Jhon Don</span>
-						<span class="block ml-2 text-sm text-gray-600">25 minutes</span>
-					</div>
-					<span class="block ml-2 text-sm text-gray-600">bye</span>
-				</div>
-			</a>
-			<a
-				class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out bg-gray-100 border-b border-gray-300 cursor-pointer focus:outline-none">
-				<img class="object-cover w-10 h-10 rounded-full"
-					src="https://cdn.pixabay.com/photo/2016/06/15/15/25/loudspeaker-1459128__340.png" alt="username" />
-				<div class="w-full pb-2">
-					<div class="flex justify-between">
-						<span class="block ml-2 font-semibold text-gray-600">Same</span>
-						<span class="block ml-2 text-sm text-gray-600">50 minutes</span>
-					</div>
-					<span class="block ml-2 text-sm text-gray-600">Good night</span>
-				</div>
-			</a>
-			<a
-				class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none">
-				<img class="object-cover w-10 h-10 rounded-full"
-					src="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg" alt="username" />
-				<div class="w-full pb-2">
-					<div class="flex justify-between">
-						<span class="block ml-2 font-semibold text-gray-600">Emma</span>
-						<span class="block ml-2 text-sm text-gray-600">6 hour</span>
-					</div>
-					<span class="block ml-2 text-sm text-gray-600">Good Morning</span>
-				</div>
-			</a>
-		</li>}
-	</For>
 
 	return (
 		<div class="container mx-auto">
@@ -67,45 +27,24 @@ const ChatPage: Component = () => {
 					<ul class="overflow-auto h-[32rem]">
 						<h2 class="my-2 mb-2 ml-2 text-lg text-gray-600">Conversations</h2>
 						<li>
-							<a
-								class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none">
-								<img class="object-cover w-10 h-10 rounded-full"
-									src="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg" alt="username" />
-								<div class="w-full pb-2">
-									<div class="flex justify-between">
-										<span class="block ml-2 font-semibold text-gray-600">Jhon Don</span>
-										<span class="block ml-2 text-sm text-gray-600">25 minutes</span>
+							<For each={conversations()}>
+								{(conv) => <a class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none"
+								// onclick={}
+								>
+									<img class="object-cover w-10 h-10 rounded-lg" src={conv.user?.avatarUrl ?? forum} alt="username" />
+									<div class="w-full pb-2">
+										<div class="flex justify-between">
+											<span class="block ml-2 font-semibold text-gray-600">{conv.name}</span>
+											<span class="block ml-2 text-sm text-gray-600">{conv.lastTime}</span>
+										</div>
+										<span class="block ml-2 text-sm text-gray-600">{conv.messages[conv.messages.length - 1].message}</span>
 									</div>
-									<span class="block ml-2 text-sm text-gray-600">bye</span>
-								</div>
-							</a>
-							<a
-								class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out bg-gray-100 border-b border-gray-300 cursor-pointer focus:outline-none">
-								<img class="object-cover w-10 h-10 rounded-full"
-									src="https://cdn.pixabay.com/photo/2016/06/15/15/25/loudspeaker-1459128__340.png" alt="username" />
-								<div class="w-full pb-2">
-									<div class="flex justify-between">
-										<span class="block ml-2 font-semibold text-gray-600">Same</span>
-										<span class="block ml-2 text-sm text-gray-600">50 minutes</span>
-									</div>
-									<span class="block ml-2 text-sm text-gray-600">Good night</span>
-								</div>
-							</a>
-							<a
-								class="flex items-center px-3 py-2 text-sm transition duration-150 ease-in-out border-b border-gray-300 cursor-pointer hover:bg-gray-100 focus:outline-none">
-								<img class="object-cover w-10 h-10 rounded-full"
-									src="https://cdn.pixabay.com/photo/2018/01/15/07/51/woman-3083383__340.jpg" alt="username" />
-								<div class="w-full pb-2">
-									<div class="flex justify-between">
-										<span class="block ml-2 font-semibold text-gray-600">Emma</span>
-										<span class="block ml-2 text-sm text-gray-600">6 hour</span>
-									</div>
-									<span class="block ml-2 text-sm text-gray-600">Good Morning</span>
-								</div>
-							</a>
+								</a>}
+							</For>
 						</li>
 					</ul>
 				</div>
+
 				<div class="hidden lg:col-span-2 lg:block">
 					<div class="w-full">
 						<div class="relative flex items-center p-3 border-b border-gray-300">
